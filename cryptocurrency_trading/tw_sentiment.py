ddb58ece4bd19d64f6c6ffe1e9403bc8d05aa34d
Yinan Li <y.li241@lse.ac.uk>
2021-09-07 00:41:16 +0800

    modified

--- cryptocurrency_trading/tw_sentiment.py
+++ cryptocurrency_trading/tw_sentiment.py
@@ -54,9 +54,27 @@ def download_model():
 
 
 
+def get_tweets_sentimentscore(df):
+    df['sentimentscore'] = ''
 
+    task = 'sentiment'
+    MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
 
-def get_tweets_sentimentscore(resampled_df):
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+    # download label mapping
+    labels = []
+    mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+    with urllib.request.urlopen(mapping_link) as f:
+        html = f.read().decode('utf-8').split("\n")
+        csvreader = csv.reader(html, delimiter='\t')
+    labels = [row[1] for row in csvreader if len(row) > 1]
+
+    # PT
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    model.save_pretrained(MODEL)
+    tokenizer.save_pretrained(MODEL)
+
 
     def preprocess(text):
         new_text = []
@@ -88,13 +106,14 @@ def get_tweets_sentimentscore(resampled_df):
 
         return (scores[2]-scores[0])/scores[1]
 
-    resampled_df['senntimentscore']=''
 
-    for i in range(10):
-        resampled_df['sentimentscore'][i]=sentimentscore(resampled_df.text[i])
+
+    for i in range(len(df)):
+        df['sentimentscore'][i]=sentimentscore(df.text[i])
         print(i)
 
-    return resampled_df
+    return df
+
 
 
 def get_daily_sentimentscore(df):
