86d23d498c0e021a5c02c1ebbaf8ad1b0bc9bbf4
Claudia Lobato <clobato7@gmail.com>
2021-09-06 17:56:03 +0100

    Started function to scrape live data

---
+++ cryptocurrency_trading/data.py
@@ -0,0 +1,30 @@
+from red_sentiment import all_reddit
+from technical_analysis import all_tech_analysis
+import pandas as pd
+import requests
+
+
+def get_price_data():
+    env_variables = dotenv_values(".env")
+    api_key = env_variables['FINANCIAL_MODELLING_API_KEY']
+    symbol='BTCUSD'
+    query=f'https://financialmodelingprep.com/api/v3/historical-price-full/{symbol}?apikey={api_key}'
+    response=requests.get(query)
+    df = pd.DataFrame(response.json()['historical'])
+    df['date'] = pd.to_datetime(df['date'])
+    df_2 = df[['date', 'adjClose']]
+    date = pd.to_datetime('2018-09-01')
+    df3years = df_2[df_2['date']>date]
+    return df3years
+
+
+def get_all_data():
+
+    df = get_price_data()
+    reddit_data = all_reddit(start_date, end_date)
+    tech_data = all_tech_analysis(df)
+
+
+
+
+
